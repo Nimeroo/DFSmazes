b3555aff029f65612ac4682ef11c87ef3009871e
Nimeroo <girou.joe@gmail.com>
2021-09-12 07:05:19 -0400

    Maze class and grid section class created

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const maze = document.getElementById("maze");
 const ctx = maze.getContext("2d");
+
+class Maze {
+    constructor(rows, columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.grid = [];
+        this.array = [];
+    }
+}
+
+class Section{
+    constructor(parentGrid, rowIndex, columnIndex){
+        this.parentGrid = parentGrid;
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;
+        this.visited = false;
+        this.sides = {
+            top: true,
+            bottom: true,
+            right: true,
+            left: true,
+        }
+    }
+}
